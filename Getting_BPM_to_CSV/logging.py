eb1592900286da870db1d1418cf66e8e4b74024f
Adam Parsons <adam.a.parsons15@gmail.com>
2025-06-04 12:37:53 +0100

    added timestamp of each heartbeat

--- Getting_BPM_to_CSV/logging.py
+++ Getting_BPM_to_CSV/logging.py
@@ -1,10 +1,10 @@
 import serial
-from datetime import datetime
+from datetime import datetime, timedelta
 import csv
 
 #Open a csv file and set it up to receive comma delimited input
 logging = open('logging.csv',mode='a')
-writer = csv.writer(logging, delimiter=",",lineterminator="\n", quoting=csv.QUOTE_NONE)
+writer = csv.writer(logging, delimiter=",",lineterminator="\n", quoting=csv.QUOTE_MINIMAL)
 
 #Open a serial port that is connected to an Arduino (below is Linux, Windows and Mac would be "COM4" or similar)
 #No timeout specified; program will wait until all serial data is received from Arduino
@@ -17,6 +17,14 @@ ser.flushInput()
 #This character tells the Arduino to start sending data
 ser.write(bytes('x', 'utf-8'))
 
+datetime_ser = datetime.now()
+print(datetime_ser)
+#runs once at first send, gets time at first run
+
+ser_recieved = ser.readline()
+decoded_bytes = ser_recieved.decode("utf-8").strip().split(',')
+last_millis = int(decoded_bytes[1])
+
 
 while True:
     #Read in data from Serial until \n (new line) received
@@ -26,17 +34,32 @@ while True:
     #Convert received bytes to text format
     decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
     print(decoded_bytes)
-    #Retreive current time
-    c = datetime.now()
-    current_time = c.strftime('%H:%M:%S.%f')[:-3]
-    print(current_time)
+
+    decoded_bytes = decoded_bytes.split(',')
+    
+    ser_bytes = decoded_bytes[0] #Splits bpm from corregated string
+    ser_millis = int(decoded_bytes[1]) #Time in millis from arduino start
+    
+    #difference in milliseconds between current and last data sent
+    diff_millis = ser_millis - last_millis
+    last_millis = ser_millis
+
+    #print(ser_bytes)
+
+    #adds the difference in ms to the current time
+    datetime_ser += timedelta(milliseconds=float(diff_millis))
+    
+    print(datetime_ser)
+
+    temp_time = datetime_ser.strftime('%H:%M:%S.%f')[:-3]
+
     
     #If Arduino has sent a string "stop", exit loop
     if (decoded_bytes == "stop"):
         break
     
     #Write received data to CSV file
-    writer.writerow([current_time,decoded_bytes])
+    writer.writerow([datetime_ser,decoded_bytes])
             
 # Close port and CSV file to exit
 ser.close()
