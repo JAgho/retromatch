4dc434445bff87e2674441fc1338dfc299931386
Adam Parsons <adam.a.parsons15@gmail.com>
2025-06-05 11:15:36 +0100

    binary converter python done

--- Getting_BPM_to_CSV/binary reader.py
+++ Getting_BPM_to_CSV/binary reader.py
@@ -2,10 +2,13 @@ import struct
 
 
 with open("binary_stream.bin", "rb") as binary_file:
-    lines = binary_file.readlines()
-    for i in lines:
-        print(i)
-        signal, timestamp, funnyno = struct.unpack('<LLL', i)
+    while True:
+        chunk = binary_file.read(12)  # Read 12 bytes (3 unsigned longs)
+        if len(chunk) < 12:
+            break  # EOF reached or incomplete chunk
+
+        signal, timestamp, funnyno = struct.unpack('<LLL', chunk)
+        print(f"Signal: {signal}, Funny Number: {funnyno}, Time: {timestamp}")
 
 
 
